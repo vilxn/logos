573e629deaea11fef94762d3ef5f73c04dbb55b7
vilxn <ibrahimamankelby@gmail.com>
2026-04-26 17:02:49 +0500

    register

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -5,10 +5,18 @@ interface LoginPayload { email: string; password: string; }
 interface LoginResponse { token: string; }
 
 interface RegisterPayload { email: string; password: string; firstname: string; lastname: string; role: string; }
+interface RegisterResponse { email: string; id: number; token: string; }
 
 export const authApi = {
   login: async (payload: LoginPayload): Promise<void> => {
     const { data } = await client.post<LoginResponse>('/auth/login', payload);
+    tokenStorage.set(data.token);
+  },
+
+  register: async (payload: RegisterPayload): Promise<void> => {
+    const { data } = await client.post<RegisterResponse>('/auth/register', payload);
+    localStorage.setItem("email", data.email);
+    localStorage.setItem("id", data.id.toString());
     tokenStorage.set(data.token);
   },
 
